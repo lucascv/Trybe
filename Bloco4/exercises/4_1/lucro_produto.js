ab75ed09a0ef1095615ae33d09b987fb1650705d
Lucas Costa Ventura <lucascv333@gmail.com>
2020-05-12 19:07:26 -0300

    add toFixed(2) no exercicio lucro_produto

--- Bloco4/exercises/4_1/lucro_produto.js
+++ Bloco4/exercises/4_1/lucro_produto.js
@@ -3,4 +3,4 @@ let vendaProduto = 40;
 let quantidadeVendida = 1000;
 let imposto = custoProduto * 0.2
 
-console.log((40 - (20 + imposto)) * quantidadeVendida)
+console.log(((40 - (20 + imposto)) * quantidadeVendida).toFixed(2))
