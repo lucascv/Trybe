df06b7a68d26c0048203e1aa5748930feff75706
Lucas Costa Ventura <lucascv333@gmail.com>
2020-05-15 19:16:38 -0300

    add ex9 dia 4.4

---
+++ Bloco4/exercises/4_4/ex9.js
@@ -0,0 +1,16 @@
+// Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de 
+// caracteres.
+
+array = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+
+function verificaMaiorNome(array) {
+  let maiorNome = 'A';
+  for (index in array) {
+    if (array[index].length >= maiorNome.length) {
+      maiorNome = array[index];
+    }
+  }
+  return maiorNome;
+}
+
+console.log(verificaMaiorNome(array));
