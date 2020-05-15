031b6ac6268c473aa52a7109343c86f036af34ec
Lucas Costa Ventura <lucascv333@gmail.com>
2020-05-15 18:25:55 -0300

    add ex6 dia 4.4

---
+++ Bloco4/exercises/4_4/ex6.js
@@ -0,0 +1,29 @@
+// Crie uma função que receba uma string e retorne true se for um palíndromo , ou false, 
+// se não for.
+
+function verificaPalindromo(palavra) {
+  let diferencas = 0;
+  let n = 1;
+  for (let i = 0; i < palavra.length; i+=1) {
+    if (palavra[i] !== palavra[palavra.length - n]) {
+      n += 1;
+      diferencas += 1;
+    } 
+    else {
+      n += 1;
+    }
+  }
+  if (diferencas > 0) {
+    return "NÃO é palíndromo."
+  }
+  else {
+    return "Palíndromo."
+  }
+}
+
+console.log(verificaPalindromo("arara"));
+console.log(verificaPalindromo("desenvolvimento"));
+console.log(verificaPalindromo("ana"));
+console.log(verificaPalindromo("reviver"));
+console.log(verificaPalindromo("mussum"));
+console.log(verificaPalindromo("brasil"));
