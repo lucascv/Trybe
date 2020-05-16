8643e2cf20a17b76e714c5c44703adee5439fb43
Lucas Costa Ventura <lucascv333@gmail.com>
2020-05-15 21:40:32 -0300

    add verificacao na funcao do ex12 dia 4.4

--- Bloco4/exercises/4_4/ex12.js
+++ Bloco4/exercises/4_4/ex12.js
@@ -3,15 +3,20 @@
 // string word.
 
 function verificaFimPalavra(stringWord, stringEnding) {
-  let fimIgual = true;
-  for (let i = 1; i <= stringEnding.length; i += 1) {
-    if (stringWord[stringWord.length - i] !== stringEnding[stringEnding.length - i]) {
-      fimIgual = false;
-    }
+  if (stringWord.length > stringEnding.length) {
+    let fimIgual = true;
+    for (let i = 1; i <= stringEnding.length; i += 1) {
+      if (stringWord[stringWord.length - i] !== stringEnding[stringEnding.length - i]) {
+        fimIgual = false;
+      }
+    }  
+    return fimIgual;
   }
-
-  return fimIgual;
+  else {
+    return 'StringWord não é maior que a StringEnding'
+  }
 }
 
 console.log(verificaFimPalavra("trybe", "be"));
 console.log(verificaFimPalavra("joaofernando", "fernan"));
+console.log(verificaFimPalavra("be", "trybe"));
