6c0fdcd0701b30af22bb8af914f9cf5fdd7ea2a2
Lucas Costa Ventura <lucascv333@gmail.com>
2020-05-16 14:10:25 -0300

    corrigindo bonus dia 4.4

--- Bloco4/exercises/4_4/bonus.js
+++ Bloco4/exercises/4_4/bonus.js
@@ -15,8 +15,8 @@ function transformaRomanoEmInteiro(numRomano) {
   let numInteiro = [];
   let sum = 0;
   
-  for (i = 0; i < numRomano.length; i += 1) {
-    if (numRomano[i] in romanos) {
+  for (let i = 0; i < numRomano.length; i += 1) {
+    if (numRomano[i].toUpperCase() in romanos) {
       numInteiro[i] = romanos[numRomano[i]];
     }
     else {
@@ -24,30 +24,31 @@ function transformaRomanoEmInteiro(numRomano) {
     }
   }
 
-  if (numInteiro[numInteiro.length - 2] < numInteiro[numInteiro.length - 1]) {
-    for (i = 0; i < numInteiro.length; i += 1) {
-      if (i === numInteiro.length - 2) {
-        sum -= numInteiro[i];
-
+  for (let n = 0; n < numInteiro.length; n += 1) {
+    if (n < numInteiro.length - 1) {
+      if (numInteiro[n] < numInteiro[n + 1]) {
+        sum -= numInteiro[n];
       }
       else {
-        sum += numInteiro[i];
+        sum += numInteiro[n];
       }
     }
-  }
-  else {
-    for (index in numInteiro) {
-      sum += numInteiro[index];
-    }
+    else {
+      sum += numInteiro[n];
+    }    
   }
   return sum;
 }
 
-console.log(transformaRomanoEmInteiro('XI'));
-console.log(transformaRomanoEmInteiro('XXX'));
-console.log(transformaRomanoEmInteiro('MXI'));
-console.log(transformaRomanoEmInteiro('IX'));
-console.log(transformaRomanoEmInteiro('IV'));
-console.log(transformaRomanoEmInteiro('CM'));
-console.log(transformaRomanoEmInteiro('A'));
-console.log(transformaRomanoEmInteiro('XIV'));
+let vetorRomano = ['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV',
+'XV','XVI','XVII','XVIII','XIX','XX','XXI','XXII','XXIII','XXIV','XXV','XXVI','XXVII','XXVIII',
+'XXIX','XXX','XXXI','XXXII','XXXIII','XXXIV','XXXV','XXXVI','XXXVII','XXXVIII','XXXIX','XL',
+'XLI','XLII','XLIII','XLIV','XLV','XLVI','XLVII','XLVIII','XLIX','L','LI','LII','LIII','LIV',
+'LV','LVI','LVII','LVIII','LIX','LX','LXI','LXII','LXIII','LXIV','LXV','LXVI','LXVII','LXVIII',
+'LXIX','LXX','LXXI','LXXII','LXXIII','LXXIV','LXXV','LXXVI','LXXVII','LXXVIII','LXXIX','LXXX',
+'LXXXI','LXXXII','LXXXIII','LXXXIV','LXXXV','LXXXVI','LXXXVII','LXXXVIII','LXXXIX','XC','XCI',
+'XCII','XCIII','XCIV','XCV','XCVI','XCVII','XCVIII','XCIX','C'];
+
+for (const key in vetorRomano) { 
+  console.log(transformaRomanoEmInteiro(vetorRomano[key]));
+}
