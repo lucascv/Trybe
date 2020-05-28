318a4ac43080037d832e55b6053adef565e3db73
Lucas Costa Ventura <lucascv333@gmail.com>
2020-05-28 18:55:24 -0300

    criando funcao dos estados

---
+++ Bloco6/exercises/6_1/script.js
@@ -0,0 +1,32 @@
+// Função que cria as opções do ComboBox com os estados do Brasil
+const function criarEstados() {
+  let estadosBrasileiros = {
+    ac: 'Acre',
+    al: 'Alagoas',
+    ap: 'Amapá',
+    am: 'Amazonas',
+    ba: 'Bahia',
+    ce: 'Ceará',
+    es: 'Espírito Santo',
+    go: 'Goiás',
+    ma: 'Maranhão',
+    mt: 'Mato Grosso',
+    ms: 'Mato Grosso do Sul',
+    mg: 'Minas Gerais',
+    pa: 'Pará',
+    pb: 'Paraíba',
+    pr: 'Parańa',
+    pe: 'Pernambuco',
+    pi: 'Piauí',
+    rj: 'Rio de Janeiro',
+    rn: 'Rio Grande do Norte',
+    rs: 'Rio Grande do Sul',
+    ro: 'Rondônia',
+    rr: 'Roraima',
+    sc: 'Santa Catarina',
+    sp: 'São Paulo',
+    se: 'Sergipe',
+    to: 'Tocantins',
+    df: 'Distrito Federal'
+  }          
+}
