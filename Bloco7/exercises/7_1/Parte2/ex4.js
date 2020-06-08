601145fba03637d94d6cdbefeb0a49f4d72476a5
Lucas Costa Ventura <lucascv333@gmail.com>
2020-06-08 14:51:56 -0300

    add ex4 parte 2

---
+++ Bloco7/exercises/7_1/Parte2/ex4.js
@@ -0,0 +1,14 @@
+const function1 = (string) => {
+  const frase = `Tryber ${string} aqui`;
+  return frase;
+};
+
+console.log(function1('bebeto'));
+
+const skills = ['javascript', 'html', 'css', 'excel', 'trade'];
+
+const function2 = (frase) => {
+  
+};
+
+console.log(function2);
