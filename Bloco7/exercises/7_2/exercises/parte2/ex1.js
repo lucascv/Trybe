e4dfdb1a358dc637637b6298d08627edfccfc643
Lucas Costa Ventura <lucascv333@gmail.com>
2020-06-08 19:06:04 -0300

    add ex5 parte 2

--- Bloco7/exercises/7_2/exercises/parte2/ex1.js
+++ Bloco7/exercises/7_2/exercises/parte2/ex1.js
@@ -57,3 +57,14 @@ const funcao3 = (objeto) => {
 }
 
 console.log(funcao3(lesson2));
+
+// 5- Crie um objeto de nome allLessons, que deve agrupar todas as aulas através do 
+// Object.assign. Cada chave desse novo objeto será uma aula, sendo essas chaves: 
+// lesson1, lesson2 e lesson3. Ao executar o comando console.log(allLessons), a saída deverá 
+// ser a seguinte:
+
+let allLessons = Object.assign({}, {lesson1}, {lesson2}, {lesson3});
+console.log(allLessons);
+
+// 6- Usando o objeto criado no exercício 5, crie uma função que retorne o número total de 
+// estudantes em todas as aulas.
