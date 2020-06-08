63cf36f6363fe527caa8f2a85a92e6d93f49bdd5
Lucas Costa Ventura <lucascv333@gmail.com>
2020-06-08 18:26:49 -0300

    add ex1 parte 1

---
+++ Bloco7/exercises/7_2/exercises/parte1/ex1.js
@@ -0,0 +1,12 @@
+// Setup
+var testObj = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas"
+  };
+  
+  // Only change code below this line
+  
+  let playerNumber = 16;     // Change this line
+  let player = testObj[playerNumber];   // Change this line
+  
