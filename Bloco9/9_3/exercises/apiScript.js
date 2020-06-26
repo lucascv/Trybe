69fbc20a5863730f5f36d80e8110d8ac50178089
Lucas Costa Ventura <lucascv333@gmail.com>
2020-06-26 12:41:09 -0300

    add ex1

--- Bloco9/9_3/exercises/apiScript.js
+++ Bloco9/9_3/exercises/apiScript.js
@@ -1,7 +1,14 @@
 const API_URL = 'https://icanhazdadjoke.com/';
 
+const myObject = {
+  method: 'GET',
+  headers: { 'Accept': 'application/json' }
+};
+
 const fetchJoke = () => {
-  // Adicionar lógica aqui!
+  fetch(API_URL, myObject)
+  .then(response => response.json())
+  .then(data => document.querySelector('#jokeContainer').innerHTML = data.joke);
 };
 
 window.onload = () => fetchJoke();
