4f0dd1f2d5621583e37c9c4ae9f949bd349674e5
Lucas Costa Ventura <lucascv333@gmail.com>
2020-06-26 13:35:47 -0300

    add ex3

--- Bloco9/9_3/exercises/promise.js
+++ Bloco9/9_3/exercises/promise.js
@@ -1,10 +1,13 @@
-// Agora, um passo para trás: vamos fazer, passo a passo, uma Promise. Primeiramente, 
+// 2- Agora, um passo para trás: vamos fazer, passo a passo, uma Promise. Primeiramente, 
 // instancie uma Promise. Dentro dela, você deve produzir um array com dez números aleatórios 
 // de 1 a 50 e elevá-los todos ao quadrado. Se a soma de todos esses elementos for inferior a 
 // 8000, a promise deve ser resolvida. Caso contrário, ela deve ser rejeitada. Acresça um then 
 // e um catch à Promise com qualquer coisa dentro só para que o código funcione.
 
-/* Resolvendo com FOR
+/* 
+
+// Resolvendo com FOR
+
 new Promise((resolve, reject) => {
   const arr = [];
   let sum = 0;
@@ -17,7 +20,6 @@ new Promise((resolve, reject) => {
   else reject('Soma menor que 8000');
 
 })    
-*/
 
 // Resolvendo com HOF's
 
@@ -29,4 +31,19 @@ new Promise((resolve, reject) => {
   else reject(console.log('Erro!'));
 })
 
+*/
 
+// 3- Quando a promise for resolvida com sucesso, retorne o resultado da divisão desse número 
+// por 2, 3, 5 e 10 em um array.
+
+new Promise((resolve, reject) => {
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const arr2 = arr.map(num => num = Math.floor(Math.random() * 50) ** 2);
+  const sum = arr2.reduce((a, b) => a + b);
+  if (sum < 8000) {
+    const arr3 = [2, 3, 5, 10];
+    const resultado = arr3.map(num => num = sum / num);
+    resolve(console.log(resultado));
+  }  
+  else reject(console.log('Erro!'));
+})
