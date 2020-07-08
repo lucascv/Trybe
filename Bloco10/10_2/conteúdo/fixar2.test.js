2b3095636fb991835062163a6478bee05bc0cb11
Lucas Costa Ventura <lucascv333@gmail.com>
2020-07-08 02:03:30 -0300

    add fixar - funcao dividir

--- Bloco10/10_2/conteúdo/fixar2.test.js
+++ Bloco10/10_2/conteúdo/fixar2.test.js
@@ -35,4 +35,24 @@ test('Teste da função somar', () => {
   expect(math.somar(1, 2)).toBe(3);
   expect(math.somar).toHaveBeenCalled();
   expect(math.somar).toHaveBeenLastCalledWith(1, 2);
+});
+
+// 4- Faça o mock da função dividir e implemente um retorno padrão com o valor '15'. 
+// Implemente também os seguintes valores para a primeira e segunda chamadas: '2' e '5'. 
+// Teste a chamada, o retorno, os parâmetros e quantas vezes a função foi chamada.
+
+test('Teste da função dividir', () => {
+  math.dividir = jest
+    .fn()
+    .mockReturnValue(15)
+    .mockReturnValueOnce(2)
+    .mockReturnValueOnce(5);
+
+  expect(math.dividir()).toBe(2);
+  expect(math.dividir).toHaveBeenCalled();
+  expect(math.dividir()).toBe(5);
+  expect(math.dividir()).toBe(15);
+  expect(math.dividir).toHaveBeenCalledTimes(3);
+
 })
+
