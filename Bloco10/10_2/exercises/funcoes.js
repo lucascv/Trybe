0761d6621d372c4733272f923053f97784bd58e3
Lucas Costa Ventura <lucascv333@gmail.com>
2020-07-08 13:01:02 -0300

    add ex4

--- Bloco10/10_2/exercises/funcoes.js
+++ Bloco10/10_2/exercises/funcoes.js
@@ -1,3 +1,9 @@
 const randomNumber = () => Math.floor(Math.random()*101);
 
-module.exports = { randomNumber }
+const uppercase = string => string.toUpperCase();
+
+const firstWord = string => string[0];
+
+const sumStrings = (string1, string2) => string1 + string2;
+
+module.exports = { randomNumber, uppercase, firstWord, sumStrings }
