25f789fe00e2132c895e068f40b2d1dda2acaf82
Lucas Costa Ventura <lucascv333@gmail.com>
2020-07-07 22:35:19 -0300

    add ex1

--- Bloco10/10_1/exercises/ex1.test.js
+++ Bloco10/10_1/exercises/ex1.test.js
@@ -2,13 +2,12 @@
 // que transforma as letras de uma palavra em letras maiúsculas. Lembre-se de ter 
 // cuidado com os falso-positivos em testes assíncronos.
 
-const uppercase = (str, callback) => {
-  callback(str.toUpperCase());
-}
+const lib = require('./funcoes')
+
 
 describe('exercicio 10-1', () => {
   test('Testando se função retorna o esperado', (done) => {
-    uppercase('teste', (callback) => {
+    lib.uppercase('teste', (callback) => {
       expect(callback).toBe('TESTE');
       done();
     });
---
+++ Bloco10/10_1/exercises/funcoes.js
@@ -0,0 +1,28 @@
+// 1
+
+const uppercase = (str, callback) => {
+  callback(str.toUpperCase());
+}
+
+// 2 e 3
+
+const users = {
+    4: { name: 'Mark' },
+    5: { name: 'Paul' }
+  };
+  
+const findUserById = (id) => {
+  return new Promise((resolve, reject) => {
+      if (users[id]) {
+        return resolve(users[id]);
+      }
+
+      return reject({ error: 'User with ' + id + ' not found.' });
+  });
+}
+  
+const getUserName = (userID) => {
+  return findUserById(userID).then(user => user.name);
+}
+
+module.exports = { uppercase, findUserById, getUserName }
