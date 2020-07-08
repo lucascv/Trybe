e3d04b01916b0433efb265f47773e0e5bbf12208
Lucas Costa Ventura <lucascv333@gmail.com>
2020-07-08 12:08:55 -0300

    add ex2

--- Bloco10/10_2/exercises/ex1.test.js
+++ Bloco10/10_2/exercises/ex1.test.js
@@ -1,4 +1,8 @@
-const math = require('./ex1');
+// 1- Crie uma função que gere um número aleatório entre 0 e 100. Você irá criar também os 
+// testes para essa função. Utilizando o mock, defina o retorno padrão como 10. Teste se a 
+// função foi chamada, qual seu retorno e quantas vezes foi chamada.
+
+const math = require('./funcoes');
 
 test('ex1', () => {
   math.randomNumber = jest
