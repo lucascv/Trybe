04ed8955fbc93e41af4806e954d79cffd2cec24a
Lucas Costa Ventura <lucascv333@gmail.com>
2020-07-07 23:02:31 -0300

    add ex2

--- Bloco10/10_1/exercises/funcoes.js
+++ Bloco10/10_1/exercises/funcoes.js
@@ -7,17 +7,17 @@ const uppercase = (str, callback) => {
 // 2 e 3
 
 const users = {
-    4: { name: 'Mark' },
-    5: { name: 'Paul' }
-  };
+  4: { name: 'Mark' },
+  5: { name: 'Paul' }
+};
   
 const findUserById = (id) => {
   return new Promise((resolve, reject) => {
-      if (users[id]) {
-        return resolve(users[id]);
-      }
+    if (users[id]) {
+      return resolve(users[id]);
+    }
 
-      return reject({ error: 'User with ' + id + ' not found.' });
+    return reject({ error: 'User with ' + id + ' not found.' });
   });
 }
   
