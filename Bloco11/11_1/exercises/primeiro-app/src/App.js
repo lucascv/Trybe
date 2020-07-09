518a7761447d47bc45851b9419682ae3531eb100
Lucas Costa Ventura <lucascv333@gmail.com>
2020-07-09 19:18:54 -0300

    add ex primeiro-app

--- Bloco11/11_1/exercises/primeiro-app/src/App.js
+++ Bloco11/11_1/exercises/primeiro-app/src/App.js
@@ -8,22 +8,14 @@ const task = (value) => {
   );
 }
 
+const compromissos = ['acordar', 'comer', 'dormir']
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      {compromissos.map(compromisso => task(compromisso))}
       </header>
     </div>
   );
