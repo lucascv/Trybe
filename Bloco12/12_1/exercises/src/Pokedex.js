4f50fb53c3ed1e5e80bc1279c58ce3187517f568
Lucas Costa Ventura <lucascv333@gmail.com>
2020-07-17 01:01:51 -0300

    add ex dia 12.1

--- Bloco12/12_1/exercises/src/Pokedex.js
+++ Bloco12/12_1/exercises/src/Pokedex.js
@@ -1,18 +1,43 @@
 import React from 'react';
-import Pokemons from './data';
+import pokemons from './data';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      index: 0,
+      type: 'All'
+    }
+  }
+
+  changePokemon = () => this.state.index === (pokemons.length - 1) ? this.setState({ index: 0})
+  : this.setState({ index: this.state.index + 1});
+
+  typeFilter = () => this.state.type === 'All' ? pokemons 
+  : pokemons.filter((pokemon) => pokemon.type === this.state.type);
+
+  changeType = (event) => this.setState({ type: event.target.value });
+
   render() {
-    return Pokemons.map(pok => (
-      <Pokemon
-      name={pok.name}
-      type={pok.type}
-      image={pok.image}
-      averageWeight={pok.averageWeight.value}
-      measurementUnit={pok.averageWeight.measurementUnit}
-      />
-    ))
+    const lista = this.typeFilter();
+    return (
+      <section>
+        <Pokemon
+          name={lista[this.state.index].name}
+          type={lista[this.state.index].type}
+          image={lista[this.state.index].image}
+          averageWeight={lista[this.state.index].averageWeight.value}
+          measurementUnit={lista[this.state.index].averageWeight.measurementUnit}
+        />
+        <button onClick={this.changePokemon}>Next Pokémon</button>
+        <div>
+          <button value="Fire" onClick={this.changeType}>Fire</button>
+          <button value="Psychic" onClick={this.changeType}>Psychic</button>
+          <button value="All" onClick={this.changeType}>All</button>
+        </div>
+      </section>
+    )
   }
 }
 
