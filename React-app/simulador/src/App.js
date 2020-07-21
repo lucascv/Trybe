b0dbedeee83612643d022ad19cb77fe593dc6039
Lucas Costa Ventura <lucascv333@gmail.com>
2020-07-21 19:20:44 -0300

    add ex3

--- React-app/simulador/src/App.js
+++ React-app/simulador/src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import Home from './Home';
+import { Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
+      <div>
         <Home />
+        <Link to="/" > Home </Link>
+        <li><Link to="/about" /> About </li>
+        <li><Link to="/users" /> Users </li>
+      </div>
     );
   }
 }
--- React-app/simulador/src/index.js
+++ React-app/simulador/src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
 import { BrowserRouter } from "react-router-dom";
+import { Link } from "react-router-dom";
+
 
 ReactDOM.render(
   <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <Link to="/" > Home </Link>
+    <Link to="/about" > About </Link>
+    <Link to="/users" > Users </Link>
+    <App />
   </BrowserRouter>,
   document.getElementById('root')
 );
