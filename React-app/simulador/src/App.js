aa92299594c535f9d7c2250a32126b9beb47dfe1
Lucas Costa Ventura <lucascv333@gmail.com>
2020-07-21 20:10:08 -0300

    add ex6 e ex7

--- React-app/simulador/src/App.js
+++ React-app/simulador/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Home from './Home';
 import About from "./About";
+import Users from "./Users";
 
 import { Switch, Route, Link } from "react-router-dom";
 
@@ -21,6 +22,7 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={Home}/>
             <Route path="/about" component={About}/>
+            <Route path="/users" component={Users}/>
           </Switch>
         </main>
 
