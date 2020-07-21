9c99eb79711e32d2371bddb4613cf937946a4876
Lucas Costa Ventura <lucascv333@gmail.com>
2020-07-20 21:42:03 -0300

    subindo pratica do dia

---
+++ React-app/simulador/src/Pictures.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Pictures = (props) => {
+  const { height, src, alt, legenda } = props;
+  return (
+    <div>
+      <img src={src} alt={alt} height={height} />
+      <p>{legenda}</p>
+    </div>
+  );
+}
+
+export default Pictures;
+
+Pictures.propTypes = {
+  alt: PropTypes.string.isRequired,
+  height: PropTypes.number,
+  src: PropTypes.string.isRequired,
+};
+
+Pictures.defaultProps = {
+  height: 400,
+};
