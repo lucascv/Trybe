26ac6092e54fdf922090c21b18a4b66a0d42e9a9
Lucas Costa Ventura <lucascv333@gmail.com>
2020-07-20 23:51:46 -0300

    add ex 2

--- React-app/simulador/src/App.js
+++ React-app/simulador/src/App.js
@@ -1,12 +1,42 @@
 import React, { Component } from 'react';
+import Button from './Button';
+import Alert from './Alert';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      showModal: false,
+      isDisableButton: false,
+    }
+  }
+
+  changeTitle = (value) => {
+    this.setState({ title: value })
+  }
+
+  changeShowComponent = () => {
+    this.setState((state) => (
+      {
+        showModal: !state.showModal,
+        isDisableButton: !state.isDisableButton
+      }
+    ))
+  }
+
   render() {
     return (
       <div className='main'>
-        <p>teste</p>
+        <Button content="Clique aqui" isDisable={this.state.isDisableButton} showComponent={this.changeShowComponent} value='Título Show' />
+        {this.state.showModal &&
+          <Alert
+            hideComponent={this.changeShowComponent}
+            contentTitle="Modal"
+            content="Coloque qualquer coisa aqui."
+          />}
       </div>
-    );
+    )
   }
 }
 
