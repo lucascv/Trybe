5bb58987a9bffb2130765a5190258fa89e64fab7
Lucas Costa Ventura <lucascv333@gmail.com>
2020-08-07 13:59:48 -0300

    add ex1

---
+++ Bloco16/16_1/exercises/ex1.js
@@ -0,0 +1,11 @@
+const action = {
+  type: 'LOGIN'
+}
+// Define an action creator here:
+
+function actionCreator(state, action) {
+  switch (action.type) {
+    case 'LOGIN':
+      return
+  }
+}
